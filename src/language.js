bf71fe73c5e7d6adb43990439da8e710d7cbe2b9
Giovanni Pelloso <giovanni_pelloso@hotmail.com>
2022-01-17 02:47:51 -0300

    site

--- src/language.js
+++
@@ -1,3 +0,0 @@
-/* eslint-disable */
-export let LANGUAGE = localStorage.getItem("language") !== null && localStorage.getItem("language") !== undefined ? localStorage.getItem("language") : 'pt-BR';
-export function setLANGUAGE(language) {LANGUAGE = language};
